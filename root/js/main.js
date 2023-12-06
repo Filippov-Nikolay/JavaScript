2a2daa979c2d9a4196cb3c4563f57d2d5461d606
Filippov-Nikolay <filippovnikolay195@gmail.com>
2023-12-06 18:55:14 +0200

    hw7

--- root/js/main.js
+++ root/js/main.js
@@ -1,40 +1,26 @@
-function example1() {
-    let a = 15;
-
-    console.log(`Ваш возраст - ${a}`);
-}
-function example2() {
-    let a;
-    let b = a ?? "Hello world";
-
-    console.log(b); // Hello world
-}
-function example3() {
-    let a = 10;
-    let b = a ?? "Hello world";
-
-    console.log(b); // 10
-}
-function example4() {
-    let a = prompt("Введите данные: ");
-    console.log(a);
-}
-function example5() {
-    let a = confirm("Вы уверенны...?");
-    console.log(a);
-}
-function example6() {
-    alert("Hello wolrd");
-}
-function example7() {
-    let a = "10";
-    let b = 10;
-    if (a === b) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-}
-
-
 // Подсветка кода
+let code = document.querySelectorAll(".example-code code");
+for(let i = 0; i < code.length; i++) {
+    let temp = code[i].innerHTML;
+    let nameVar = "";
+
+    console.log(temp.indexOf("="));
+    console.log(temp.indexOf("let"));
+
+    for (let j = temp.indexOf("let") + 4; j < temp.indexOf("=") - 1; j++) {
+        nameVar += temp[j];
+    }
+
+    console.log(nameVar);
+    console.log(temp);
+
+    temp = temp.replace(/let/g, "<span class='variable'>let</span>")
+    .replace(/nameVar/g, `<span class='variable-name'>${nameVar}</span>`)
+    .replace(/if/g, "<span class='operator'>if</span>")
+    .replace(/console/g, "<span class='object'>console</span>")
+    .replace(/log/g, "<span class='method'>log</span>");
+
+    code[i].innerHTML = temp;
+
+    console.log(temp);
+}
